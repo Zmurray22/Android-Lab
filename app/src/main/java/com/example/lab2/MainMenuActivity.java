25c236d90a2607bba49500e26bed7f69aface9d8
Zmurray22 <88351080+Zmurray22@users.noreply.github.com>
2021-11-20 22:33:13 -0600

    Added test image page

--- app/src/main/java/com/example/lab2/MainMenuActivity.java
+++ app/src/main/java/com/example/lab2/MainMenuActivity.java
@@ -2,7 +2,10 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        //Add a listener to the button to take you to the temp converter
+        Button btnWeather=(Button) findViewById(R.id.btnWeather);
+        btnWeather.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View v) {
+                                              goWeather();
+                                          }
+                                      }
+        );
+
+        //Add a listener to the button to take you to the image activity
+        Button btnDraw=(Button) findViewById(R.id.btnDraw);
+        btnDraw.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View v) {
+                                              goDraw();
+                                          }
+                                      }
+        );
+    }
+
+    private void goWeather() {
+        Intent intent = new Intent(MainMenuActivity.this, WeatherActivity.class);
+        this.startActivity(intent);
+    }
+
+    private void goDraw() {
+        Intent intent = new Intent(MainMenuActivity.this, MyDrawingActivity.class);
+        this.startActivity(intent);
     }
 }
--- app/src/main/java/com/example/lab2/WeatherActivity.java
+++ app/src/main/java/com/example/lab2/WeatherActivity.java
@@ -2,7 +2,9 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,13 +26,20 @@ public class WeatherActivity extends AppCompatActivity implements RadioGroup.OnC
         btnConvert.setOnClickListener(this);
     }
 
-    private void displayToast(double temperature){
-        if(temperature > 50)
-            Toast.makeText(this, getString(R.string.tempToastF), Toast.LENGTH_LONG).show();
-        else if(temperature > 20)
-            Toast.makeText(this, getString(R.string.tempToastC), Toast.LENGTH_LONG).show();
-        else
-            Toast.makeText(this, getString(R.string.tempToastK), Toast.LENGTH_LONG).show();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch(item.getItemId()) {
+            case R.id.mnuMain:
+                startActivity(new Intent(getApplicationContext(), MainMenuActivity.class));
+                return true;
+            case R.id.mnuExit:
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -110,5 +119,14 @@ public class WeatherActivity extends AppCompatActivity implements RadioGroup.OnC
         }else{
             displayToast(answer);
         }
+    }
+
+    private void displayToast(double temperature){
+        if(temperature > 50)
+            Toast.makeText(this, getString(R.string.tempToastF), Toast.LENGTH_LONG).show();
+        else if(temperature > 20)
+            Toast.makeText(this, getString(R.string.tempToastC), Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, getString(R.string.tempToastK), Toast.LENGTH_LONG).show();
     }
 }
